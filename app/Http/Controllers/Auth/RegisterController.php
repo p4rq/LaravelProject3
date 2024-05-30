b3fa04493c314a53f7d051761cf485772931cdc4
p4rq <anuarbek0471@gmail.com>
2024-05-30 21:01:39 +0500

    123456

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
     |--------------------------------------------------------------------------
     | Register Controller
     |--------------------------------------------------------------------------
-    |
+    |12
     | This controller handles the registration of new users as well as their
     | validation and creation. By default this controller uses a trait to
     | provide this functionality without requiring any additional code.
